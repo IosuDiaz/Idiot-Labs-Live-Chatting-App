1dd1e171f87c67fbc1c3ed3bd38cd01040c976e5
IosuDiaz <68014673+IosuDiaz@users.noreply.github.com>
2025-03-15 02:24:58 -0300

    chore(server): action cable set up (#17)
    * chore(server): action cable set up
    
    * feat(server): add another handler in connection

--- server/app/channels/application_cable/connection.rb
+++ server/app/channels/application_cable/connection.rb
@@ -1,4 +1,27 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      reject_unauthorized_connection unless current_user&.confirmed?
+    end
+
+    private
+
+    def find_verified_user
+      token = request.params[:token] || find_token_from_header
+      return unless token
+
+      decoded_token = JwtService.decode(token)
+      User.find_by!(id: decoded_token["user_id"])
+    rescue JWT::DecodeError, JWT::ExpiredSignature, ActiveRecord::RecordNotFound
+      nil
+    end
+
+    def find_token_from_header
+      header = request.headers["Authorization"]
+      header&.split("Bearer ")&.last
+    end
   end
 end
