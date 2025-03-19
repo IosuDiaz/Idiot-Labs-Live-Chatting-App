2e040902b346ed17481f473730296e94f3858aa5
IosuDiaz <iosufdiaz@gmail.com>
2025-03-18 22:29:11 -0300

    feat(client): add user login and registration

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  showResendModal: boolean = false;
+  resendEmail: string = '';
+
+  private errorMessages: Record<number, string> = {
+    400: 'Solicitud inválida. Verifique los datos ingresados.',
+    401: 'Credenciales inválidas.',
+    404: 'El email no existe.',
+    422: 'El usuario ya se encuentra confirmado.',
+  };
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Por favor, complete todos los campos';
+      return;
+    }
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+        if (response.token) {
+          localStorage.setItem('authToken', response.token);
+        }
+        this.router.navigate(['']);
+      },
+      error: (response) => {
+        this.errorMessage = this.errorMessages[response.status] || 'Ocurrió un error inesperado.';
+
+        console.error('Error en el login:', response);
+      },
+    });
+  }
+
+  goToSignup() {
+    this.router.navigate(['/signup']);
+  }
+
+  openResendModal() {
+    this.showResendModal = true;
+  }
+
+  closeResendModal() {
+    this.showResendModal = false;
+    this.resendEmail = '';
+  }
+
+  resendConfirmationLink() {
+    if (!this.resendEmail) {
+      this.errorMessage = 'Por favor ingrese un email.';
+      return;
+    }
+
+    this.authService.resendConfirmationLink(this.resendEmail).subscribe({
+      next: () => {
+        alert('Link de confirmación reenviado.');
+        this.closeResendModal();
+      },
+      error: (response) => {
+        this.errorMessage = this.errorMessages[response.status] || 'Ocurrió un error inesperado.';
+
+        this.closeResendModal();
+
+        console.error('Error al reenviar link de confirmación:', response);
+      },
+    });
+  }
+}
---
+++ client/src/app/signup/signup.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent {
+  nickname: string = '';
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, public router: Router) {} // 
+
+  createAccount() {
+    const user = {
+      nickname: this.nickname,
+      email: this.email,
+      password: this.password,
+    };
+  
+    this.authService.signup(user).subscribe({
+      next: (response) => {
+        console.log('Usuario registrado con éxito:', response);
+        this.router.navigate(['/login']);
+      },
+      error: (response) => {
+        this.errorMessage = 'Hubo un error al crear la cuenta.';
+        console.error('Error al crear la cuenta:', response);
+        
+        if (response.error.errors) {
+          this.fieldErrors = response.error.errors;
+        }
+      },
+    });
+  }
+  
+  fieldErrors: { [key: string]: string[] } = {};
+}
