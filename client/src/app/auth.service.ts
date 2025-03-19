c5c52129700e836ad571106bf013d59839d75695
IosuDiaz <68014673+IosuDiaz@users.noreply.github.com>
2025-03-18 22:49:45 -0300

    feat(client): add user login and registration (#28)
    * feat(client): add user login and registration
    
    * fix(client): add end newline to files

---
+++ client/src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  signup(user: { nickname: string, email: string, password: string }): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signup`, {user: user});
+  }
+
+  resendConfirmationLink(email: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/users/resend_confirmation`, { email });
+  }
+}
