b7f737a0ce56c94d4c54a91ffd2e93b15dcb83ec
IosuDiaz <iosufdiaz@gmail.com>
2025-03-19 21:09:38 -0300

    chore(client): token validation in app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,27 @@ export class AppComponent implements OnInit {
   title = 'Live Chatting App';
   searchQuery: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
-    const token = localStorage.getItem('authToken');
+    const token = localStorage.getItem('authToken') || '';
+
     if (!token) {
       this.router.navigate(['/login']);
     }
+
+    this.authService.getUser(token).subscribe({
+      next: (response) => {
+        if (response.status == '200') {
+          localStorage.setItem('authToken', response.data.access_token);
+        }
+        this.router.navigate(['']);
+      },
+      error: () => {
+        localStorage.removeItem('authToken');
+        this.router.navigate(['/login']);
+      },
+    });
   }
 
   get showNavbar(): boolean {
--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 
@@ -21,5 +21,12 @@ export class AuthService {
 
   resendConfirmationLink(email: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/users/resend_confirmation`, { email });
+  }
+
+  getUser(token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    return this.http.get(`${this.apiUrl}/users`, { headers });
   }
 }
